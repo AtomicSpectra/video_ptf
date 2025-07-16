7ca9c806a297234cfe4976eddc895d984000a995
AtomicSpectra <chrishnakartik@gmail.com>
2025-07-16 06:17:37 +0530

    dev_started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import connectDB from "../db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
+
 dotenv.config({
     path:'./.env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`listning on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECTION FAILED",err)
+})
