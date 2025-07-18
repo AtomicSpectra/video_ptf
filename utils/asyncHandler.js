7ddd5e37adc287cc547fac675ff743a5622a2a3f
AtomicSpectra <chrishnakartik@gmail.com>
2025-07-18 23:28:32 +0530

    connect

---
+++ utils/asyncHandler.js
@@ -0,0 +1,26 @@
+//const asyncHandler=()=>{}
+//const asyncHandler=(func)=>{()=>{}}
+//const asyncHandler=(func)=>()=>{}
+
+import { Promise } from "mongoose"
+
+/*
+const asyncHandler=(fn)=>async(req,res,next)=>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code||500).json({
+            success:false,
+            message: error.message 
+        })
+    }
+}
+*/
+
+const asyncHandler=(fn)=>{
+    (req,res,next)=>{
+         Promise.resolve(fn(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
